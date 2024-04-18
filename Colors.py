d000f464f4aa33be78df218cc128a9a62f5221a7
Adi Daniel <124789363+AdiDanielx@users.noreply.github.com>
2024-04-18 20:03:31 +0300

    Create Colors.py
    colors

---
+++ Colors.py
@@ -0,0 +1,9 @@
+
+class bcolors:
+    OKBLUE = '\033[94m'
+    BOLD = '\033[1m'
+    HEADER = '\033[95m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    RED = '\033[31m'
+    YELLOW = '\033[33m'
