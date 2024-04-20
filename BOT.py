4715c5c6958c2942a8763096259879456a42c913
Adi Daniel <124789363+AdiDanielx@users.noreply.github.com>
2024-04-20 17:51:39 +0300

    Update BOT.py
    add documents

--- BOT.py
+++ BOT.py
@@ -3,10 +3,21 @@ import socket
 import struct
 
 class BOT(BasePlayer):
+    """
+    BOT class that inherits from BasePlayer, tailored to operate as a bot within the game.
+    
+    This subclass is specifically designed to act as a bot in networked game sessions,
+    automatically responding to game interactions using predefined logic rather than human input.
+    """
     def __init__(self):
-        super().__init__(bot=True)
+        """
+        Initialize the BOT class by invoking the initializer of the BasePlayer with a bot flag.
+        
+        This constructor sets up the BOT as a bot player, enabling automatic behaviors for game interactions.
+        """
+        super().__init__(bot=True) # Call the parent class's initializer with the bot flag set to True
 
 
 if __name__ == "__main__":
-    B = BOT()
-    B.play()
+    B = BOT() # Instantiate a BOT object
+    B.play()   # Start the bot's participation in the game
