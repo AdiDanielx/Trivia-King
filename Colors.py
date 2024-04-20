03e3aa13ec39f498f9021be54e8eb5dffbabfb69
Adi Daniel <124789363+AdiDanielx@users.noreply.github.com>
2024-04-20 23:23:59 +0300

    Create Colors.py
    Colors

---
+++ Colors.py
@@ -0,0 +1,26 @@
+
+class bcolors:
+    OKBLUE = '\033[94m'
+    BOLD = '\033[1m'
+    HEADER = '\033[95m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    GREEN = '\x1b[38;5;49m'
+    RED = "\x1b[38;5;202m"
+    YELLOW = '\x1b[38;5;229m'
+    WHITE = '\033[37m' 
+    LIGHT_PINK =  "\u001b[38;5;201m"
+    LEVENDER =  "\u001b[38;5;147m"
+    AQUA = "\u001b[38;2;145;231;255m"
+    PENCIL = "\u001b[38;2;253;182;0m"
+    WHITE_CYAN = "\u001b[201;37m"
+    RED_YELLOW = "\u001b[31;43m"
+    UNDERLINE = "\u001b[4m"
+    ITALICIZE = "\u001b[3m"
+    LIGHTPURPLE = "\x1b[38;5;222m"
+    background_cyan = "\x1b[46m"
+    light = "\x1b[38;5;244m"
+    PINK = "\x1b[38;5;225m"
+    LIGHT2= "\x1b[38;5;231m"
+
+    
