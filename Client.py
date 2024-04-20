dbc5ddc7649648b10264c411ebb685ab6c193292
Adi Daniel <124789363+AdiDanielx@users.noreply.github.com>
2024-04-20 17:40:46 +0300

    Update Client.py
    with docstring

--- Client.py
+++ Client.py
@@ -4,9 +4,17 @@ import struct
 
 class Client(BasePlayer):
     def __init__(self):
-        super().__init__()
+        """
+        Initializes a new instance of the Client class by inheriting from BasePlayer.
+        
+        This constructor calls the constructor of the BasePlayer to initialize the client-specific
+        properties and setups required for the client to function properly in the context of a networked
+        game or application. This might include setting up network connections, initializing game-specific
+        data, and other client-specific initializations.
+        """
+        super().__init__() # Call the initializer of the base class BasePlayer
 
 
 if __name__ == "__main__":
-    C = Client()
-    C.play()
+    C = Client() # Create an instance of Client
+    C.play()  # Start the client's functionality, such as playing a game or other activities defined in BasePlayer
